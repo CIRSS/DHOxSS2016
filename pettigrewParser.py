e76b9c86795506bd7e5f94431b9a1dd6ea41ffb5
akthom <andrea.thomer@gmail.com>
2016-06-18 16:58:00 -0700

    update - nltk now in loop

--- pettigrewParser.py
+++ pettigrewParser.py
@@ -9,15 +9,10 @@ import nltk, re, pprint, csv
 infile="pettigrewSubset.txt"
 # index="/Users/thomer2/GitHub/DHOxSS2016/TJPindexClean.csv"
 
-
 with open(infile, 'r') as myfile:
 	datas=myfile.read()
 	datas=datas.split('\n\n\n')
-
-# with open(index,'r') as indexfile:
-#	d1=csv.reader(indexfile)
 	
-
 outfile = open("someData.csv","a")
 w=csv.writer(outfile)
 
@@ -25,14 +20,12 @@ indBox=[]
 allBoxes=[]
 
 for i in datas:
-
 	if i.startswith("Box"):
 		allBoxes.append(indBox)
 #		print(allBoxes)
 		indBox=[]
 		indBox.append(i)
 #		print(indBox)
-		
 		
 	else:
 		indBox.append(i)
@@ -53,7 +46,6 @@ indLetter=[]
 allLetters=[]
 countyMcCountFace=0
 
-
 for box in allBoxes:
 	if box ==[]:
 		pass
@@ -61,26 +53,57 @@ for box in allBoxes:
 		letters=box[1::]
 		for i in letters:
 			indLetter=[box[0]]
+			tokens=word_tokenize(i)
+			text=nltk.Text(tokens)
+			coll=text.collocations()
+			V=set(text)
+			long_words = [w for w in V if len(w) > 10]
 			indLetter.append(i)
+			indLetter.append(coll)
+			indLetter.append(long_words)			
 			allLetters.append(indLetter)
 			countyMcCountFace=countyMcCountFace+1
 			print(countyMcCountFace)
 			print(indLetter)
-			
 			indLetter=[]
+print ('found ', len(allLetters), ' of Pettigrew\'s papers!')
 
-# 	if len(i)==2:
-# 		indRow=[]
-# 		indRow=i
-# 		allRows.append(i)
-# 	else:
-# 		
-# 		indRow.append(i[0]).append(i[1]).append(i[next])
-	
 
-# print (allBoxes[0])
-# print (allBoxes[1])
+# 
+# len(TJPtext)
+# tokens=word_tokenize(TJPtext)
+# text=nltk.Text(tokens)
+# text.collocations()
+# def lexical_div(text):
+#	return len(set(text))/len(text)
+# long_words = [w for w in V if len(w) > 10]
+# sorted(long_words)
+# fdist=FreqDist(text)
+# sorted(w for w in set(text)if len(w)>2 and fdist[w]>2
+# nltk.pos_tag(text)
+# tokenized_text = word_tokenize(text)
+# classified_text = st.tag(tokenized_text)
+# 
+# print(classified_text)
+
 		
+
+
+
+
+# st = StanfordNERTagger('/Users/thomer2/stanford-ner-2015-12-09/classifiers/english.all.3class.distsim.crf.ser.gz','/Users/thomer2/stanford-ner-2015-12-09/stanford-ner.jar',encoding='utf-8') 
+# 
+# text = 'While in France, Christine Lagarde discussed short-term stimulus efforts in a recent interview with the Wall Street Journal.'
+# 
+TJPtext = '''My Dear Sir, 
+I only received your favour of the 16th late last night, on my return from the Syro-Egyptian, or I would have answered before.  
+I shall be very happy to meet Mr Wright at the Archaeological meeting this evening, the meeting of which society indeed, I have attended on the last occasion.  
+Believe me, 
+My dear sir, 
+Yours very sincerely, 
+W. Francis Ainsworth
+Wednesday Morn
+Dec 17th 1845'''
 	
 # 	print(allBoxes)
 # 		while status =0
@@ -107,76 +130,3 @@ for box in allBoxes:
 #		print ("this is data!!! and I will put it in a row!")
 #		w.writerow([boxMeta[0]],[boxMeta[1]],[i])
 
-
-
-
-# with open(index, 'wb') as csvfile:
-#	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-#	for row in spamreader:
-#		print (', '.join(row))
-
-# for i in range(10,20):
-#	if 'Box' in m[i]:
-# ...	print("yuppers " + m[i])
-# ...  else:
-# ...	continue
-	
-# counter = 0
-# 
-# with open(infile) as p:
-#	counter += 1
-#	filename="pettigrew"+str(counter)
-
-
-# can break each letter on "BOX?"	
-
-# https://pythonprogramming.net/named-entity-recognition-stanford-ner-tagger/
-
-
-
-
-
-# with open(index, 'wb') as csvfile:
-#	spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-#	for row in spamreader:
-#		print (', '.join(row))
-
-# csv.register_dialect(
-#	  'mydialect',
-#	  delimiter = ',',
-#	  quotechar = '"',
-#	  doublequote = True,
-#	  skipinitialspace = True,
-#	  lineterminator = '\r\n',
-#	  quoting = csv.QUOTE_MINIMAL)
- #this and next commented section from https://www.getdatajoy.com/examples/python-data-analysis/read-and-write-a-csv-file-with-the-csv-module - trying to write to a damn csv!
-
-# st = StanfordNERTagger('/Users/thomer2/stanford-ner-2015-12-09/classifiers/english.all.3class.distsim.crf.ser.gz','/Users/thomer2/stanford-ner-2015-12-09/stanford-ner.jar',encoding='utf-8') 
-# 
-# text = 'While in France, Christine Lagarde discussed short-term stimulus efforts in a recent interview with the Wall Street Journal.'
-# 
-# TJPtext = '''My Dear Sir, 
-# I only received your favour of the 16th late last night, on my return from the Syro-Egyptian, or I would have answered before.  
-# I shall be very happy to meet Mr Wright at the Archaeological meeting this evening, the meeting of which society indeed, I have attended on the last occasion.  
-# Believe me, 
-# My dear sir, 
-# Yours very sincerely, 
-# W. Francis Ainsworth
-# Wednesday Morn
-# Dec 17th 1845'''
-# 
-# len(TJPtext)
-# tokens=word_tokenize(TJPtext)
-# text=nltk.Text(tokens)
-# text.collocations()
-# def lexical_div(text):
-#	return len(set(text))/len(text)
-# long_words = [w for w in V if len(w) > 10]
-# sorted(long_words)
-# fdist=FreqDist(text)
-# sorted(w for w in set(text)if len(w)>2 and fdist[w]>2
-# nltk.pos_tag(text)
-# tokenized_text = word_tokenize(text)
-# classified_text = st.tag(tokenized_text)
-# 
-# print(classified_text)
