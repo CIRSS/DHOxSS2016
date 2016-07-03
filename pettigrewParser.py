a0e4fbe9f0d63a585807439b9114c5e914c94242
akthom <andrea.thomer@gmail.com>
2016-07-03 14:41:20 +0100

    cleaning up script

--- pettigrewParser.py
+++ pettigrewParser.py
@@ -74,32 +74,7 @@ with open("pettigrewData.csv","w",newline="") as w:
 	writer=csv.writer(w)
 	writer.writerows(allLetters)
 
-# 
-# len(TJPtext)
-# tokens=word_tokenize(TJPtext)
-# text=nltk.Text(tokens)
-# text.collocations()
-# def lexical_div(text):
-#	return len(set(text))/len(text)
-# long_words = [w for w in V if len(w) > 10]
-# sorted(long_words)
-# fdist=FreqDist(text)
-# sorted(w for w in set(text)if len(w)>2 and fdist[w]>2
-# nltk.pos_tag(text)
-# tokenized_text = word_tokenize(text)
-# classified_text = st.tag(tokenized_text)
-# 
-# print(classified_text)
 
-		
-
-
-
-
-# st = StanfordNERTagger('/Users/thomer2/stanford-ner-2015-12-09/classifiers/english.all.3class.distsim.crf.ser.gz','/Users/thomer2/stanford-ner-2015-12-09/stanford-ner.jar',encoding='utf-8') 
-# 
-# text = 'While in France, Christine Lagarde discussed short-term stimulus efforts in a recent interview with the Wall Street Journal.'
-# 
 TJPtext = '''My Dear Sir, 
 I only received your favour of the 16th late last night, on my return from the Syro-Egyptian, or I would have answered before.  
 I shall be very happy to meet Mr Wright at the Archaeological meeting this evening, the meeting of which society indeed, I have attended on the last occasion.  
@@ -110,28 +85,3 @@ W. Francis Ainsworth
 Wednesday Morn
 Dec 17th 1845'''
 	
-# 	print(allBoxes)
-# 		while status =0
-# 			if "Box" in maybetext:
-# 				status=1
-# 			else:
-# 				w.writerow([folder,name,maybetext])
-# 				next=datas.index(i)+1
-# 				maybetext=datas[next]
-# 			continue
-# 		print(text)
-
-
-
-#		text=datas.next()
-#		w.writerow([folder,name])
-		
-#		else:
-#			boxMeta.append[i]
-#			print(boxMeta)
-# 
-#		continue #or pass works equally well
-#	else:
-#		print ("this is data!!! and I will put it in a row!")
-#		w.writerow([boxMeta[0]],[boxMeta[1]],[i])
-
